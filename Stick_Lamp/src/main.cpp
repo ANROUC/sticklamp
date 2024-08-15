60ff973f9d53a9aee5698e0e715605a3197307a2
ANROUC <91256846+ANROUC@users.noreply.github.com>
2024-08-15 23:52:09 +0300

    Add files via upload

---
+++ Stick_Lamp/src/main.cpp
@@ -0,0 +1,115 @@
+#define PIN D7
+#define NUM_LEDS 64
+
+#define AP_SSID "WikiLink-AC3D"
+#define AP_PASS "f15cbb4e"
+
+#include <GyverHub.h>
+#include "Adafruit_NeoPixel.h"
+#include "Color_set.h"
+
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(NUM_LEDS, PIN, NEO_GRB + NEO_KHZ800);
+GyverHub hub("MyDevice", "ESP8266", ""); // имя сети, имя устройства, иконка
+
+int Click_Pwr_btn;
+int Power_Status;
+
+int Color_count; // Текстовая переменная для выбора элемента массива
+bool flag = 0;   // Переменная обработка флага прерывания
+
+void fill_start()
+{
+  strip.clear();
+  for (int a = 0; a < NUM_LEDS; a++)
+  {
+    strip.setPixelColor(a, colors[Color_count]);
+    strip.show();
+    delay(10);
+  }
+  flag = 1;
+  Power_Status = 1;
+}
+
+void power_on()
+{
+  Click_Pwr_btn = 1;
+    if (Power_Status == Click_Pwr_btn)
+  {
+    loop();
+  }
+  else
+  {
+    fill_start();
+  }
+}
+
+void power_off()
+{
+  Click_Pwr_btn = 0;
+  Serial.println(Click_Pwr_btn);
+  if (Power_Status == Click_Pwr_btn)
+  {
+    loop();
+  }
+  else
+  {
+    strip.clear();
+    strip.show();
+    Power_Status = 0;
+  }
+}
+
+
+// билдер
+void build(gh::Builder &b)
+{
+  b.Title("Control Panel");
+  b.beginRow(); // начать
+  b.Button_("PowerOn").icon("f011").color(gh::Colors::Green).attach(power_on);
+  b.Button_("PowerOff").icon("f011").color(gh::Colors::Red).attach(power_off);
+  b.endRow();
+
+  b.beginRow();
+  b.Slider().color(gh::Colors::Green);
+  b.endRow();
+}
+
+
+void setup()
+{
+  Serial.begin(9600);
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(AP_SSID, AP_PASS);
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(1000);
+    Serial.print("Connecting... ");
+  }
+  Serial.println();
+  Serial.println(WiFi.localIP());
+
+  strip.begin();
+  strip.setBrightness(250);
+
+  hub.config(F("MyDevices"), F("Stick_Lapm_1"), F(""));
+  hub.onBuild(build); // подключаем билдер
+  hub.begin();
+}
+
+
+void loop()
+{
+  hub.tick();
+  Color_count = 1;
+
+  if (flag == 0)
+  {
+    fill_start();
+  }
+
+  static gh::Timer tmr(1000); // период 1 секунда
+  if (tmr)
+  {
+    hub.update(F("title")).value(millis());
+  }
+}
